ad993f9588461035155809114c86655dab6e2058
dsandersAzure <dsanders_can@outlook.com>
2017-11-28 14:06:41 -0500

    Removed old and deprecated files

--- deprecated/v2/showSpinup.sh
+++
@@ -1,8 +0,0 @@
-function showSpinup {
-    echo >&2
-    echo "|------------------------------------------------------------------|" >&2
-    echo "| Deploying Python Cowbull suite with the following options        |" >&2
-    echo "|------------------------------------------------------------------|" >&2
-    echo >&2
-    return 0
-}
--- deprecated/v3/showWelcome.sh
+++
@@ -1,7 +0,0 @@
-function showWelcome {
-    echo >&2
-    echo "===============================================================" >&2
-    echo "     Python Cowbull Game - Control and Installation Script  ">&2
-    echo "===============================================================" >&2
-    echo >&2
-}
